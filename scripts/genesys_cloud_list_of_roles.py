bad15dba95d9883642f0a4acb54aea7e27ddb0ee
Ioffe Ben <ben.ioffe@aol.com>
2021-11-06 12:31:41 -0400

    Generate a list of roles

---
+++ scripts/genesys_cloud_list_of_roles.py
@@ -0,0 +1,50 @@
+import base64, requests, sys, os
+
+CLIENT_ID = '<GENESYS_CLOUD_CLIENT_ID>'
+CLIENT_SECRET = '<GENESYS_CLOUD_CLIENT_SECRET>'
+ENVIRONMENT = 'mypurecloud.com'
+
+# Base64 encode the client ID and client secret
+authorization = base64.b64encode(bytes(CLIENT_ID + ":" + CLIENT_SECRET, "ISO-8859-1")).decode("ascii")
+
+# Prepare for POST /oauth/token request
+request_headers = {
+    "Authorization": f"Basic {authorization}",
+    "Content-Type": "application/x-www-form-urlencoded"
+}
+request_body = {
+    "grant_type": "client_credentials"
+}
+
+# Get token
+response = requests.post(f"https://login.{ENVIRONMENT}/oauth/token", data=request_body, headers=request_headers)
+
+# Check response
+if response.status_code == 200:
+    print("Got token")
+else:
+    print(f"Failure: { str(response.status_code) } - { response.reason }")
+    sys.exit(response.status_code)
+
+# Get JSON response body
+response_json = response.json()
+
+# Prepare for GET /api/v2/authorization/roles request
+requestHeaders = {
+    "Authorization": f"{ response_json['token_type'] } { response_json['access_token']}"
+}
+
+# Get roles
+response = requests.get(f"https://api.{ENVIRONMENT}/api/v2/authorization/roles", headers=requestHeaders)
+
+# Check response
+if response.status_code == 200:
+    print("Got roles")
+else:
+    print(f"Failure: { str(response.status_code) } - { response.reason }")
+    sys.exit(response.status_code)
+
+# Print roles
+print("\nRoles:")
+for entity in response.json()["entities"]:
+    print(f"  { entity['name'] }")
